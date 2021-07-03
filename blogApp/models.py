24a2856a68b4d85ecdf8b9ddef035c179712b5b3
NIKESH PAKUWAL <pakuwalnikesh@gmail.com>
2021-07-03 13:40:37 +0545

    Style Blog and Add Blog Post

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from froala_editor.fields import FroalaField
+from django.urls import reverse
 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = FroalaField()
 
     def __str__(self) -> str:
         return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('home', args = (str(self.id))) 
