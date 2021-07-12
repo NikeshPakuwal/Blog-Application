05375c2254ebde2bc37b6c54205b43cd7c0f28ca
NIKESH PAKUWAL <pakuwalnikesh@gmail.com>
2021-07-12 22:12:41 +0545

    Customize User profile Model

--- blogApp/models.py
+++ blogApp/models.py
@@ -34,3 +34,11 @@ class Post(models.Model):
     def get_absolute_url(self):
         # return reverse('article-detail', args = (str(self.id)))
         return reverse('home')
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
